923c3d7f9a150159fa4ecab91d2081f0e38cb228
Lukasz Malucha <lucasmalucha@gmail.com>
2020-03-11 20:22:08 +0000

    data

---
+++ frontend/src/components/NotFoundComponent.vue
@@ -0,0 +1,36 @@
+<template>
+<div class="row plain-element">
+  <div id="errorContainer">
+    <div id="mainContainer">
+      <div class="company"><img src="https://www.johnsoncontrols.com/-/media/jci/be/united-states/our-brands/720/jc.png?h=320&w=720&la=en&hash=7316C1AF12DC1E1A33B842BEC63C881A472B30E8" width="280" height="120"
+                             alt="Logo Logo"/></div>
+      <br clear="all"/>
+
+      <div class="robot"><img src="/static/img/404.png" width="380" height="450" alt="Robot 404"/></div>
+
+      <div class="error-header"><span class="largeFont">Oops!</span> The Page you requested was not found</div>
+      <br clear="right"/>
+
+      <div class="mainContent">
+          <p class="p-error">HTTP 404 Error - The page you are were looking for appears <br> to have been moved, deleted or does not exist</p>
+
+          <p class="p-error"><strong>You can try:</strong></p>
+          <ul class="ul-error">
+              <li>&#8226; Visit the <a href="/">Homepage</a>
+              </li>
+              <li>&#8226; Make sure that the address is spelled correctly</li>
+              <li>&#8226; Go to <a href="javascript:history.go(-1);">previous</a> page
+              </li>
+          </ul>
+      </div>
+    </div>
+  </div>
+</div>
+</template>
+
+<script>
+
+export default {
+  name: "NotFoundComponent",
+}
+</script>
--- frontend/src/views/NotFound.vue
+++ frontend/src/views/NotFound.vue
@@ -1,28 +1,15 @@
 <template>
-<div id="page-index">
-  <div class="row row-banner row-banner-small">
-    <div class="col-md-8 left-align col-banner-small no-padding">
-      <h4>{{ message }}</h4>
-    </div>
-    <div class="col-md-4 no-padding">
-    </div>
-  </div>
-</div>
+<NotFoundComponent/>
 </template>
 
 
 <script>
-
+import NotFoundComponent from "@/components/NotFoundComponent.vue"
 
 export default {
   name: "NotFound",
   components: {
-
-  },
-  data() {
-    return {
-      message: "404 - Page Not Found"
-    }
+    NotFoundComponent
   }
 }
 </script>
