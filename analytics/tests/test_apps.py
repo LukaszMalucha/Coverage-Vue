a4ee69c5c3f8afadd85d5dc12438548fbd03d746
Lukasz Malucha <lucasmalucha@gmail.com>
2020-05-03 12:26:52 +0100

    heroku

--- analytics/tests/test_apps.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-from analytics.apps import AnalyticsConfig
-
-
-class AnalyticsConfigAppTests(TestCase):
-
-    def test_app_name(self):
-        self.assertEqual(AnalyticsConfig.name, "analytics")
--- product_search/tests/test_apps.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-
-from product_search.apps import ProductSearchConfig
-
-
-class DocumentSearchConfigAppTests(TestCase):
-
-    def test_app_name(self):
-        self.assertEqual(ProductSearchConfig.name, "product_search")
